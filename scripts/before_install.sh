e106b25821d954623749c1fb762df38c2c319290
student-shraddha <shraddhasoni2566@gmail.com>
2024-10-14 17:33:31 +0530

    Create before_install.sh

---
+++ scripts/before_install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Running Before Install Script"
+
+# Add your installation commands here
+# e.g., stopping services, cleaning up old files, etc.
+
+# Example of stopping a service
+# service myapp stop
+apt update -y
+apt upgrade -y
