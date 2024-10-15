643ee7491958465c6dcba76474746f50737ccb4e
student-shraddha <shraddhasoni2566@gmail.com>
2024-10-15 12:47:47 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -8,7 +8,7 @@ APP_DIR="/opt/codedeploy-agent/deployment-root/6b279fec-32b6-4172-ab6c-5471e5f13
 # Update and install Node.js
 sudo apt-get update -y
 curl -fsSL https://deb.nodesource.com/setup_current.x | sudo -E bash -
-sudo apt-get install -y nodejs
+sudo apt-get install nodejs -y
 
 # Change to the application directory and install npm packages
 cd "$APP_DIR"
