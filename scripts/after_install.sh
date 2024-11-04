d9f736f3ed249e12046b696cec67a1a6ba316cc6
student-shraddha <shraddhasoni2566@gmail.com>
2024-11-04 10:20:23 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -9,4 +9,4 @@ sudo yum install -y nodejs
 echo "Clearing npm cache and installing dependencies..."
 npm cache clean --force
 rm -rf node_modules package-lock.json
-npm install
+npm install --legacy-peer-deps
