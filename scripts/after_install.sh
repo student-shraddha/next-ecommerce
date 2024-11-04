56aca9624043fc792e7378f2e6a712e50e595692
student-shraddha <shraddhasoni2566@gmail.com>
2024-11-04 10:07:46 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-# Install dependencies
+
+echo "Installing Node.js and npm..."
+curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -    # For Amazon Linux 2
+# curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -    # For Ubuntu
+sudo yum install -y nodejs      # For Amazon Linux 2
+# sudo apt install -y nodejs    # For Ubuntu
+
 echo "Installing dependencies..."
-npm install --prefix /var/www/next-ecommerce
+npm install
