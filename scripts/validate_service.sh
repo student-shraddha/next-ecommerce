1e8c012708eec839710f9578ee5602ca44363427
student-shraddha <shraddhasoni2566@gmail.com>
2024-11-04 10:40:44 +0530

    Update validate_service.sh

--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
-# Check if the application is running
-if pm2 list | grep -q next-ecommerce; then
-  echo "Application is running."
-else
-  echo "Application is not running." >&2
-  exit 1
+export PATH=$PATH:/usr/local/bin:/usr/bin:/bin
+
+# Check if pm2 is installed
+if ! command -v pm2 &> /dev/null
+then
+    echo "PM2 is not installed. Installing..."
+    npm install -g pm2
 fi
 
+# Check if the application is running
+if pm2 list | grep -q "your-app-name"; then
+    echo "Application is running."
+else
+    echo "Application is not running."
+    exit 1
+fi
