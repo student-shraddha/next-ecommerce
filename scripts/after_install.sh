9c79e6ba0420d4b81f58300c1d9d571c20b90225
student-shraddha <shraddhasoni2566@gmail.com>
2024-11-04 10:12:50 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-echo "Installing Node.js and npm..."
-curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -    # For Amazon Linux 2
-# curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -    # For Ubuntu
-sudo yum install -y nodejs      # For Amazon Linux 2
-# sudo apt install -y nodejs    # For Ubuntu
+echo "Installing Node.js..."
+sudo yum install -y nodejs
 
-echo "Installing dependencies..."
+echo "Clearing npm cache and installing dependencies..."
+npm cache clean --force
+rm -rf node_modules package-lock.json
 npm install
